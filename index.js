19fa173addc5eed237af5488bd96c599c949767e
ania orwatk <orwat.ania@gmail.com>
2023-03-02 09:16:54 +0100

    finish adding tasks to localstorage

--- index.js
+++ index.js
@@ -1,38 +1,41 @@
 const form = document.querySelector("[data-form]");
-const listTasks = document.querySelector("[data-listTasks");
+const listTasks = document.querySelector("[data-listTasks]");
 const input = document.querySelector("[data-input]");
 
-let id = 1
+const getId = () => {
+    let id = Math.random() * 1000000;
+    return id
+}
 
 class Storage {
-    static addTasksStorage(tasksArr) {
-        let storage = localStorage.setItem("todo", JSON.stringify(tasksArr))
-        return storage
+    static addTodStorage(todoArr) {
+        let storage = localStorage.setItem("todo", JSON.stringify(todoArr));
+        return storage;
     }
 
     static getStorage() {
-        let storage = localStorage.getItem("task") === null ? [] : JSON.parse(localStorage.getItem("task"));
+        let storage = localStorage.getItem("todo") === null ?
+            [] : JSON.parse(localStorage.getItem("todo"));
         return storage
     }
 }
 
 let tasksArr = Storage.getStorage();
-console.log(tasksArr)
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    id += 1
+    id = getId()
     const task = new Task(id, input.value);
     tasksArr = [...tasksArr, task];
     UI.displayData();
     UI.clearInput();
-  
-    Storage.addTasksStorage(tasksArr)
-})
+    Storage.addTodStorage(tasksArr);
+});
 
 class Task {
     constructor(id, task) {
         this.id = id;
-        this.task = task
+        this.task = task;
     }
 }
 
@@ -42,35 +45,31 @@ class UI {
             return `
                 <div class="task">
                 <p>${item.task}</p>
-                <span class="remove" data-id=${item.id}>X</span>
+                <span class="remove" data-id = ${item.id}> X</span>
                 </div>
             `
-        })
-        listTasks.innerHTML = (displayData).join(" ")
+        });
+        listTasks.innerHTML = (displayData).join(" ");
     }
     static clearInput() {
-        input.value = ""
+        input.value = "";
     }
-
-    static removeTask() {
+    static removeTodo() {
         listTasks.addEventListener("click", (e) => {
             if (e.target.classList.contains("remove")) {
-                e.target.parentelement.remove()
+                e.target.parentElement.remove();
             }
-            let btnId = e.target.dataset.id
-
-            UI.removeArrayTasks(btnId)
-        })
+            let btnId = e.target.dataset.id;
+            UI.removeArrayTodo(btnId);
+        });
     }
-
-    static removeArrayTasks(id) {
-        tasksArr = tasksArr.filter((item) => { item.id !== id })
-        Storage.addTasksStorage(tasksArr)
+    static removeArrayTodo(id) {
+        tasksArr = tasksArr.filter((item) => item.id !== +id);
+        Storage.addTodStorage(tasksArr);
     }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
     UI.displayData();
-    //remove from the dom
-    UI.removeTask();
+    UI.removeTodo();
 });
